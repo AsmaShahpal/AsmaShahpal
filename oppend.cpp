473bff70146d6695a1bd93b5af4187ecdaab4a19
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-10 22:44:42 +0500

    Create oppend.cpp

---
+++ oppend.cpp
@@ -0,0 +1,16 @@
+#include<iostream>  
+using namespace std;
+int main()
+{
+	string Name = "@asmashahpal.com";
+	cout<<"My Email address "<<endl;
+	string FatherName = "	Qazi muhammad shahpal";
+	string FullName =
+	Name.append(FatherName);
+	cout<<FullName; 
+	cout<<endl;
+	cout<<FullName.length();
+
+  
+}
+//coded by asma shahpal
