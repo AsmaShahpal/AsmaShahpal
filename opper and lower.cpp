008f068011d85931ef8c388d404902aa24d0b68a
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-10 22:47:21 +0500

    Create opper and lower.cpp

---
+++ opper and lower.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	 cout<<"n\n cheak the opper and lower limits of integer:\n";
+	 cout<<"#########################\n";
+	 cout<<"*************************\n";
+	 cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+	 cout<<"The maximum limits of int data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of int data type:            "<<INT_MIN<<endl;
+	 cout<<"The maximum limits of unsigned int data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of unsigned int data type:            "<<INT_MIN<<endl;
+	 cout<<"The maximum limits of long long data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of unsigned long long data type:            "<<INT_MIN<<endl;
+	 cout<<"The maximum limits of char data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of int data type:            "<<INT_MIN<<endl;
+	 cout<<"The minimum limits of int data type:            "<<INT_MIN<<endl;
+	 cout<<"The maximum limits of int data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of int data type:            "<<INT_MIN<<endl;
+	 cout<<"The maximum limits of int data type:            "<<INT_MAX<<endl;
+	 cout<<"The minimum limits of int data type:            "<<INT_MIN<<endl;
+	 
+	 
+
+
+}
+//coded by asma shahpal
