7d55a0775184dbb986f316c0b8f12e65d6c1274d
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-10 22:45:59 +0500

    Create primitive.cpp

---
+++ primitive.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n\n cheak the wether the primitive values crossing the limits or not :\n";
+	cout<<"###################################\n";
+	cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+	cout<<"###################################\n";
+	
+	char gender='F';
+	bool isEmployed =true;
+	unsigned short numof sons =2;
+	short yearof =200;
+
+
+}
+//coded by asma shahpal
