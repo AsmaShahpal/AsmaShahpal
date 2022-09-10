9a3b5a5b8489feed65a3da20bfa33661a45748d1
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-10 22:41:52 +0500

    Create calculaterc.pp

---
+++ calculaterc.pp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int v1, v2;
+	int choice;
+	cout<<"please enter youre choice\n1:add\n2:sub\n3:mul\n4:squqre\n5:cose\n6:sin\n7:tan";
+	cin>>choice;
+	if(choice==1)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+	if (choice==2)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+    if (choice==3)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+        if (choice==4)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+     if (choice==5)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+	 if (choice==6)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+	}
+	if (choice==7)
+	{
+		cout<<"please enter v1\n";
+		cin>>v1;
+		cout<<"please enter v2\n";
+		cin>>v2;
+		cout<<"result";
+		
+}
+}
+//coded by asma shahpal
