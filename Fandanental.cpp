6576b086130c0bbef7b2a47e795878c62566756b
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-10 22:43:19 +0500

    Create Fandanental.cpp

---
+++ Fandanental.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()   
+{
+	cout<<"\n\nfind the size of fandamental data type :\n";
+	cout<<"########################\n";
+	cout<<"************************\n";
+	cout<<"########################\n";
+	cout<<"The sizeof(char)is:         "<<sizeof(char)<<"bytes is\n";
+    cout<<"The sizeof(int)is:          "<<sizeof(int)<<" bytes is\n";
+    cout<<"The sizeof(float)is:        "<<sizeof(float)<<" bytes is\n";
+    cout<<"The sizeof(double)is:       "<<sizeof(double)<<" bytes is\n";
+    cout<<"The sizeof(double Long)is:  "<<sizeof(double long)<<"bytes is\n";
+}
+//coded by asma shahpal
